64e9bb6e3f34eb0b23ab3c00ec8996eeb23caf9d
Aaron Wu <aaronwu000@gmail.com>
2024-06-13 21:57:54 +0800

    Initial Commit

--- index.js
+++ index.js
@@ -18,11 +18,70 @@ app.get("/", function (req, res) {
   res.sendFile(__dirname + '/views/index.html');
 });
 
-
 // your first API endpoint... 
-app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
+app.get("/api/:date", function (req, res) {
+
+  const url = req.params.date;
+
+  if (isValidUnixTimestamp(url)) {
+    let timestamp = parseInt(url);
+    let date = new Date(timestamp);
+    let utcTime = date.toUTCString();
+    res.json({"unix": timestamp, "utc": utcTime});
+
+  } else if (isValidDateFormat(url)) {
+    const date = new Date(url + "T00:00:00Z");
+    const unix_time = parseInt(date.getTime());
+    const utc_time = date.toUTCString();
+    res.json({"unix": unix_time, "utc": utc_time});
+  } else {
+    res.json({"error": "Invalid Date"});
+  }
 });
+
+
+function isValidUnixTimestamp(url) {
+  const regex = /(\d{13})/;
+  const match = url.match(regex);
+
+  if (match) {
+      // Parse the extracted timestamp as an integer
+      const timestamp = parseInt(match[1], 10);
+
+      // Create a Date object using the timestamp
+      const date = new Date(timestamp);
+
+      // Check if the date is valid
+      if (!isNaN(date.getTime())) {
+          return true;
+      }
+  }
+  return false;
+}
+
+function isValidDateFormat(url) {
+  // Define the regex pattern for "YYYY-MM-DD"
+  const regex = /(\d{4})-(\d{2})-(\d{2})/;
+
+  // Extract the date part from the URL
+  const match = url.match(regex);
+
+  if (match) {
+      // Extract the year, month, and day
+      const year = parseInt(match[1], 10);
+      const month = parseInt(match[2], 10);
+      const day = parseInt(match[3], 10);
+
+      // Create a Date object using the extracted values
+      const date = new Date(year, month - 1, day);
+
+      // Validate the date components
+      if (date.getFullYear() === year && date.getMonth() === month - 1 && date.getDate() === day) {
+          return true;
+      }
+  }
+  return false;
+}
 
 
 
